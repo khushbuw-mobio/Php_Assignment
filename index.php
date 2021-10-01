e0dd1cdf10cb0ee1ae331146ce54f4d35abcb928
Khushbu <Khushbuw.mobio@gmail.com>
2021-10-01 12:25:10 +0530

    "view data in index"

--- index.php
+++ index.php
@@ -51,76 +51,79 @@ require_once 'connection.php';
 <div class="col-sm-3"><span class="mdlabel">Maths</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="text" id="math" value="100 Out Of" data-name="math" class="form-control" readonly="">
+<input type="number" id="mark" name="math" class="form-control">
+
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark" name="math" class="form-control">
+<input type="text" id="math" value="Out Of 100" data-name="math" class="form-control" readonly="">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Biology</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="text" id="biology"  value="100 Out Of" class="form-control" readonly="">
+<input type="number" id="mark1" name="biology" class="form-control">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark1" name="biology" class="form-control">
+<input type="text" id="biology"  value="Out Of 100 " class="form-control" readonly="">
+
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">History</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="text" id="history"  value="100 Out Of" class="form-control" readonly="">
+<input type="number"  id="mark2" name="history"  class="form-control">
+
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number"  id="mark2" name="history"  class="form-control">
+<input type="text" id="history"  value="Out Of 100" class="form-control" readonly="">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Physics</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="text" id="english" value="100 Out Of" class="form-control" readonly="">
+<input type="number" id="mark3" name="physics" class="form-control">
 </div>
 </div>
 
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark3" name="physics" class="form-control">
+<input type="text" id="english" value="Out Of 100" class="form-control" readonly="">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Chemistry</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="text" id="chemistry" value="100 Out Of" class="form-control" readonly="">
+<input type="number" id="mark4" name="chemistry" class="form-control">
 </div>
 </div>
 
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark4" name="chemistry" class="form-control">
+<input type="text" id="chemistry" value="Out Of 100" class="form-control" readonly="">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">English</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="text" id="english" value="100 Out Of" class="form-control" readonly="">
+<input type="number" id="mark5" name="english" class="form-control">
 </div>
 </div>
 
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark5" name="english" class="form-control">
+<input type="text" id="english" value="Out Of 100" class="form-control" readonly="">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Total</span></div>
@@ -160,7 +163,7 @@ require_once 'connection.php';
     /* fetch associative array */
     while ($row = mysqli_fetch_assoc($result)) {   ?>
       <tr>
-      <td><?php echo $row['name']; ?> </td>
+      <td><a href="" class="a-link" data-toggle="modal" data-target="#myModal1"><?php echo $row['name']; ?></a> </td>
       <td><?php echo $row['perc']; ?></td>
   </tr>
         <?php } }
@@ -171,7 +174,138 @@ require_once 'connection.php';
     </div>    
       
     </div>
+    <?php
+            $sql="select * from result";
+            $result=mysqli_query($conn,$sql);
+          if ($result) {
 
+    /* fetch associative array */
+    while ($row = mysqli_fetch_assoc($result)) {   ?>
+
+    <div class="modal fade" id="myModal1" role="dialog">
+
+<div class="modal-dialog">
+<form action="save_record.php" method="post">
+  <!-- Modal content-->
+  <div class="modal-content">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal">&times;</button>
+      <h4 class="modal-title">Profile</h4>
+    </div>
+    <div class="modal-body">
+    <div class="modal-body">
+<div class="row">
+
+<div class="col-sm-3"><span class="mdlabel">Name</span></div>
+<div class="col-sm-9">
+<div class="form-group">
+<input type="text" id="name" name="name" value= "<?php echo $row['name']; ?> " class="form-control">
+</div>
+</div>
+
+<div class="col-sm-3"><span class="mdlabel">Maths</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="mark" name="math" value= "<?php echo $row['math']; ?> "  class="form-control" readonly=""> 
+
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">Marks</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="math" value="Out Of 100" data-name="math" class="form-control" readonly="">
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">Biology</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="mark1" name="biology" value= "<?php echo $row['biology']; ?> "  readonly="" class="form-control">
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">Marks</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="biology"  value="Out Of 100 " class="form-control" readonly="">
+
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">History</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text"  id="mark2" name="history" value= "<?php echo $row['history']; ?> " readonly=""  class="form-control">
+
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">Marks</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="history"  value="Out Of 100" class="form-control" readonly="">
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">Physics</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="mark3" name="physics" value= "<?php echo $row['physics']; ?> " readonly=""  class="form-control">
+</div>
+</div>
+
+<div class="col-sm-3"><span class="mdlabel">Marks</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="english" value="Out Of 100" class="form-control" readonly="">
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">Chemistry</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="mark4" name="chemistry" value= "<?php echo $row['chemistry']; ?> " readonly="" class="form-control">
+</div>
+</div>
+
+<div class="col-sm-3"><span class="mdlabel">Marks</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="chemistry" value="Out Of 100" class="form-control" readonly="">
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">English</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="mark5" name="english" value= "<?php echo $row['english']; ?> "  class="form-control" readonly="">
+</div>
+</div>
+
+<div class="col-sm-3"><span class="mdlabel">Marks</span></div>
+<div class="col-sm-3">
+<div class="form-group">
+<input type="text" id="english" value="Out Of 100" class="form-control" readonly="">
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">Total</span></div>
+<div class="col-sm-9">
+<div class="form-group">
+<input type="text" id="total" name="total" value= "<?php echo $row['total']; ?> "  class="form-control" readonly="">
+</div>
+</div>
+<div class="col-sm-3"><span class="mdlabel">Percentage</span></div>
+<div class="col-sm-9">
+<div class="form-group">
+<input type="text"  id="perc" name="perc" value= "<?php echo $row['perc']; ?> "  class="form-control" readonly="">
+</div>
+</div>
+</div>
+</div>  
+    </div>
+    <div class="modal-footer">
+      <button type="submit" name="save" class="button btn-modal-submit" data-dismiss="modal" >Close</button>
+    </div>
+  </div>
+</form>
+  
+</div>
+</div>
+<?php } }
+            ?>
     <script>
 
       $('#mark,#mark1,#mark2,#mark3,#mark4,#mark5').change(function(){

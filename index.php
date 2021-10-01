0ee3f33ee3541ad46dfbddbec70b80d9ce5015d5
Khushbu <Khushbuw.mobio@gmail.com>
2021-10-01 11:18:31 +0530

    "modified index file"

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
     <div class="header">
         <div class="header-right">
-            <a class="active" href="#home">LogOut</a>
+            <a class="active" href="logout.php">LogOut</a>
         </div>
     </div>
 
@@ -23,8 +23,9 @@
         <button class="button" data-toggle="modal" data-target="#myModal">Add record</button>
 </div>
 <div class="modal fade" id="myModal" role="dialog">
+
     <div class="modal-dialog">
-    
+    <form action="save_record.php" method="post">
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
@@ -38,7 +39,7 @@
 <div class="col-sm-3"><span class="mdlabel">Name</span></div>
 <div class="col-sm-9">
 <div class="form-group">
-<input type="text" id="name" data-name="name" class="form-control">
+<input type="text" id="name" name="name" class="form-control">
 </div>
 </div>
 
@@ -51,19 +52,19 @@
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark"  class="form-control">
+<input type="number" id="mark" name="math" class="form-control">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Biology</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="text" id="science"  value="100 Out Of" class="form-control" readonly="">
+<input type="text" id="biology"  value="100 Out Of" class="form-control" readonly="">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark1" class="form-control">
+<input type="number" id="mark1" name="biology" class="form-control">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">History</span></div>
@@ -75,7 +76,7 @@
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number"  id="mark2"  class="form-control">
+<input type="number"  id="mark2" name="history"  class="form-control">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Physics</span></div>
@@ -88,20 +89,20 @@
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark3" class="form-control">
+<input type="number" id="mark3" name="physics" class="form-control">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Chemistry</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="text" id="english" value="100 Out Of" class="form-control" readonly="">
+<input type="text" id="chemistry" value="100 Out Of" class="form-control" readonly="">
 </div>
 </div>
 
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark3" class="form-control">
+<input type="number" id="mark4" name="chemistry" class="form-control">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">English</span></div>
@@ -114,28 +115,29 @@
 <div class="col-sm-3"><span class="mdlabel">Marks</span></div>
 <div class="col-sm-3">
 <div class="form-group">
-<input type="number" id="mark3" class="form-control">
+<input type="number" id="mark5" name="english" class="form-control">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Total</span></div>
 <div class="col-sm-9">
 <div class="form-group">
-<input type="number" data-role="number" id="total" data-name="total" class="form-control" readonly="">
+<input type="number" id="total" name="total"  class="form-control" readonly="">
 </div>
 </div>
 <div class="col-sm-3"><span class="mdlabel">Percentage</span></div>
 <div class="col-sm-9">
 <div class="form-group">
-<input type="number" data-role="number" id="perc" data-name="perc" class="form-control" readonly="">
+<input type="number"  id="perc" name="perc" class="form-control" readonly="">
 </div>
 </div>
 </div>
 </div>  
         </div>
         <div class="modal-footer">
-          <button type="button" class="button btn-modal-submit" data-dismiss="modal">Save Record</button>
+          <button type="submit" name="save" class="button btn-modal-submit" >Save Record</button>
         </div>
       </div>
+    </form>
       
     </div>
   </div>
@@ -172,6 +174,25 @@
     </div>    
       
     </div>
+
+    <script>
+
+      $('#mark,#mark1,#mark2,#mark3,#mark4,#mark5').change(function(){
+      var math= parseFloat($('#mark').val()) || 0;
+      var biology =parseFloat($('#mark1').val()) || 0;
+      var history = parseFloat($('#mark2').val()) || 0;
+      var physics = parseFloat($('#mark3').val()) || 0;
+      var chamestry = parseFloat($('#mark4').val()) || 0;
+      var english = parseFloat($('#mark5').val()) || 0;
+      var total= (math+biology+history+physics+chamestry+english);
+      $('#total').val(total);
+     
+        
+        var p=(total/600) *100;
+        $('#perc').val(p.toFixed(2));    
+});
+
+      </Script>
 
 </body>
 
